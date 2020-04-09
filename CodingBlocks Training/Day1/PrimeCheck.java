2937660b105ffff027a61eead916b1ae6432fd3e
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-04-09 17:50:23 +0530

    feat: checks whether given number is prime or not

---
+++ CodingBlocks Training/Day1/PrimeCheck.java
@@ -0,0 +1,24 @@
+package Lecture1;
+
+public class PrimeCheck {
+
+	public static void main(String[] args) {
+		
+		int n = 5;
+		String result = "";
+		for (int i=2;i<=n/2;i++){
+			if (n%i == 0){
+				result = "not prime";
+				break;
+			}
+			System.out.println(i);
+			
+		}
+		if (result == "not prime"){
+			System.out.println(n+" is not prime number");
+		}else{
+			System.out.println(n+" is prime number");
+		}
+	}
+
+}
