e839212bee70087e9fc23e05f9fc981e9ca747f4
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-04-09 17:48:31 +0530

    feat: finds simple interest

---
+++ CodingBlocks Training/Day1/SimpleInterest.java
@@ -0,0 +1,13 @@
+package Lecture1;
+
+public class SimpleInterest {
+
+	public static void main(String[] args) {
+		int p = 1000;
+		int t = 1;
+		int r = 10;
+		int si = (p*t*r)/100;
+		System.out.println("simple interest is: "+ si);
+	}
+
+}
