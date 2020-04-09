f8bde5b1b8792a918b6f2d08c849ed160cc61a89
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-04-09 17:46:33 +0530

    hello world in java

---
+++ CodingBlocks Training/Day1/HelloWorld.java
@@ -0,0 +1,18 @@
+package Lecture1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world");
+		System.out.print("Coding Blocks");
+		System.out.println("Bye!");
+		System.out.print("cursor will go to new line\n");
+		System.out.print("tab experiment \t");
+		System.out.print("after tab seperated by 4 spaces\n");
+		System.out.print("coding\t");
+		System.out.print("blocks");
+
+	}
+
+}
