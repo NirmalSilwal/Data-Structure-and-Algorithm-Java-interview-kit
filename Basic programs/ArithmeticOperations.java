84cdf88e52b50ac84ab3523e65140f510ffabf03
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-06-09 01:29:49 +0530

    behaviour of division operator in different data types

---
+++ Basic programs/ArithmeticOperations.java
@@ -0,0 +1,22 @@
+package BasicPractice;
+
+public class ArithmeticOperations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double num1 = 5/2;
+		System.out.println(num1);   //2.0
+		
+		int num2 = 5/2;
+		System.out.println(num2);	//2
+		
+		double num3 = 5.0/2.0;
+		System.out.println(num3);	//2.5
+		
+		double num4 = 5.0/2;
+		System.out.println(num4);	//2.5
+	}
+
+}
+//note: Division (“/”) operates differently on integers and on doubles! 
