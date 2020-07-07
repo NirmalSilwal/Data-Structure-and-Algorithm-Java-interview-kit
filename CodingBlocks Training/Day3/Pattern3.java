185ff12de9f9646fd3f8c260daa98fb6fa45071f
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-07-07 11:33:00 +0530

    right triangular pattern

---
+++ CodingBlocks Training/Day3/Pattern3.java
@@ -0,0 +1,37 @@
+/*
+			*
+		   **
+		  ***
+		 ****
+		*****
+*/
+package Lecture3;
+
+public class Pattern3 {
+	
+	public static void main(String[] args){
+		
+		int n = 5;
+		int row = 1;
+		int nst = 1;
+		
+		while (row <= n){
+			int col = 1;
+			
+			while (col <= (n-row)){
+				System.out.print(" ");
+				col++;
+			}
+			
+			int counter = 1;
+			while (counter <= nst){
+				System.out.print("*");
+				counter++;
+			}
+			
+			System.out.println();
+			row++;
+			nst = row;
+		}
+	}
+}
