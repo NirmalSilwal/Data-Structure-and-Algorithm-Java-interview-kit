de9e3cb048c8abccedc63523bb3ffaa3ea307c89
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-07-12 18:17:39 +0530

    wave print of given 2D Array

---
+++ CodingBlocks Training/Day7/WavePrint.java
@@ -0,0 +1,38 @@
+package Lecture7;
+
+public class WavePrint {
+
+	public static void main(String[] args) {
+
+		// int[][] arr = new int[3][3];
+		// arr[0][0] = 11;
+
+		int[][] arr = { { 11, 12, 13, 14 }, { 21, 22, 23, 24 },
+				{ 31, 32, 33, 34 }, { 41, 42, 43, 44 } };
+
+		display(arr);
+		System.out.println();
+
+		// wave print logic
+		for (int i = 0; i < arr.length; i++) {
+			if (i % 2 == 0) {
+				for (int j = 0; j < arr[i].length; j++) {
+					System.out.print(arr[i][j] + " ");
+				}
+			} else {
+				for (int k = arr[i].length - 1; k >= 0; k--) {
+					System.out.print(arr[i][k] + " ");
+				}
+			}
+		}
+	}
+
+	public static void display(int[][] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = 0; j < arr[i].length; j++) {
+				System.out.print(arr[i][j] + " ");
+			}
+			System.out.println();
+		}
+	}
+}
