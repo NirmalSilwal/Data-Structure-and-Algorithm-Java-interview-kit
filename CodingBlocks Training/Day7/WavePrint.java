355a86aeeb9ef3a365d450cf2b46b69667c759b1
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-07-13 09:38:37 +0530

    added input and output example

--- CodingBlocks Training/Day7/WavePrint.java
+++ CodingBlocks Training/Day7/WavePrint.java
@@ -1,3 +1,11 @@
+/*
+input:		11 12 13 14 
+			21 22 23 24 
+			31 32 33 34 
+			41 42 43 44 
+		
+output:		11 12 13 14 24 23 22 21 31 32 33 34 44 43 42 41 
+ */
 package Lecture7;
 
 public class WavePrint {
