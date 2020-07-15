c334ee5628de55a1b4c9fd051d9257f4e0f3426d
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-07-15 10:15:34 +0530

    update: added another approach also

--- CodingBlocks Training/Day8/RightMostSetBit.java
+++ CodingBlocks Training/Day8/RightMostSetBit.java
@@ -15,23 +15,42 @@ public class RightMostSetBit {
 		System.out.println(extract_right_most_set_Bit(7)); // 1
 
 		System.out.println(extract_right_most_set_Bit(3)); // 1
+		
+		System.out.println("\nsecond approach");
+		
+		System.out.println(right_most_bit2(6));
+		System.out.println(right_most_bit2(5));
+		System.out.println(right_most_bit2(16));
+		System.out.println(right_most_bit2(7));
+		System.out.println(right_most_bit2(3));
 
 	}
-
+	
 	public static int extract_right_most_set_Bit(int num) {
 
 		int position = 1;
 		int mask = 1;
 
 		while ((num & mask) == 0) {
-			mask = mask << 1;
+			mask = mask << 1;	// left shift
 			position++;
 		}
 
 		return position;
 	}
+	
+	public static int right_most_bit2(int num){
+		int mask = 1;
+		int pos = 1;
+		
+		while ((num & mask) != 1){
+			num = num >> mask;	// right shift
+			pos++;
+		}
+		
+		return pos;
+	}
 }
-
 /*
  * Explanation: 6 is (110)in binary, here right most set bit is 1 in second
  * position
