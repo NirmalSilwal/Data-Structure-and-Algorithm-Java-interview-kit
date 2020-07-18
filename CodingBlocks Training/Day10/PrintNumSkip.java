a13dd2a47402ac6db6da8db9d041f5fe67d05f66
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-07-18 09:41:20 +0530

    prints number in skipping step recursively

---
+++ CodingBlocks Training/Day10/PrintNumSkip.java
@@ -0,0 +1,42 @@
+package Lecture10;
+
+public class PrintNumSkip {
+
+	public static void main(String[] args) {
+
+		int n = 5;
+
+		printIncDecSkip(n);
+	}
+
+	public static void printIncDecSkip(int n) {
+		if (n <= 0) {
+			return;
+		}
+		if (n % 2 != 0) {
+			System.out.println(n);
+		}
+		printIncDecSkip(n - 1);
+		if (n % 2 == 0) {
+			System.out.println(n);
+		}
+	}
+
+//	need to fix bug in this approach!
+	// public static void printIncDecSkip(int n) {
+	// if (n <= 0 ) {
+	// return;
+	// }
+	// System.out.println(n);
+	// printIncDecSkip(n - 2);
+	// System.out.println(n + 1);
+	// }
+}
+
+/*
+ * output: 5 
+		   3 
+		   1 
+		   2 
+		   4
+ */
