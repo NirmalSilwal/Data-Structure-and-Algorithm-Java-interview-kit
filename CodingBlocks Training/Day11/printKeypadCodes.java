27f27165669e1fde9f8219cada032478e9d27836
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-08-11 13:34:21 +0530

    prints all possible keypad codes/string (like in old keypad phones) from given numeric string

---
+++ CodingBlocks Training/Day11/printKeypadCodes.java
@@ -0,0 +1,61 @@
+package Lecture11;
+
+public class printKeypadCodes {
+
+	public static void main(String[] args) {
+
+		String code = "123";
+		String result = "";
+
+		printAllKeypadCodes(code, result);
+	}
+
+	public static void printAllKeypadCodes(String str, String result) {
+		if (str.equals("")) {
+			System.out.println(result);
+			return;
+		}
+
+		char fc = str.charAt(0);
+		String rest = str.substring(1);
+
+		String code = getCode(fc);
+
+		for (int i = 0; i < code.length(); i++) {
+			char letter = code.charAt(i);
+			printAllKeypadCodes(rest, result + letter);
+		}
+	}
+
+	public static String getCode(char firstChar) {
+		if (firstChar == '1') {
+			return "abc";
+		}
+		if (firstChar == '2') {
+			return "def";
+		}
+		if (firstChar == '3') {
+			return "ghi";
+		}
+		if (firstChar == '4') {
+			return "jkl";
+		}
+		if (firstChar == '5') {
+			return "mno";
+		}
+		if (firstChar == '6') {
+			return "pqr";
+		}
+		if (firstChar == '7') {
+			return "stu";
+		}
+		if (firstChar == '8') {
+			return "vwx";
+		}
+		if (firstChar == '9') {
+			return "yz";
+		}
+
+		return "";
+	}
+}
