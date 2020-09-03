cabf210368d91c7c843c9b1531d2c5c807624ba2
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-09-03 10:50:10 +0530

    inheritance demo: parent class

---
+++ CodingBlocks Training/Day16/parentInheritance.java
@@ -0,0 +1,15 @@
+package Lecture16;
+
+public class parentInheritance {
+
+	int age = 40;
+	int rollno = 7;
+
+	public void sing() {
+		System.out.println("Inside parent sing menthod");
+	}
+
+	public void read() {
+		System.out.println("Inside parent read method");
+	}
+}
