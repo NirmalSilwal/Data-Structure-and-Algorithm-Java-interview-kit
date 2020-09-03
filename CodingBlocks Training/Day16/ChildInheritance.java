03ea1fb6c6a41e7a71ef0329e7a54d74d85a7d98
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-09-03 10:50:25 +0530

    inheritance demo: child class

---
+++ CodingBlocks Training/Day16/ChildInheritance.java
@@ -0,0 +1,15 @@
+package Lecture16;
+
+public class ChildInheritance extends parentInheritance {
+
+	int age = 20;
+	String name = "Trishna";
+
+	public void read() {
+		System.out.println("Inside child read method");
+	}
+
+	public void write() {
+		System.out.println("Inside child write method");
+	}
+}
