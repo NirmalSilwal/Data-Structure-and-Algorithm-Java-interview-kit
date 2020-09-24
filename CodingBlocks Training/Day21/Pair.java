21e3cd1783c84007f6842f5329d0ad27a1e00559
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-09-24 08:47:42 +0530

    class to hold template for node height & diameter

---
+++ CodingBlocks Training/Day21/Pair.java
@@ -0,0 +1,16 @@
+package Lecture21;
+
+public class Pair {
+
+	int diameter;
+	int height;
+
+	Pair(int diameter, int height) {
+		this.diameter = diameter;
+		this.height = height;
+	}
+	
+	Pair() {
+		
+	}
+}
