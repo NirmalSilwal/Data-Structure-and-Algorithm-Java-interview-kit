9af938d5357b8c0eadeb2d278cb1ddfde4c88b56
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-09-24 13:28:30 +0530

    class to hold template for node height & isbalanced parameter

---
+++ CodingBlocks Training/Day21/PairBalanced.java
@@ -0,0 +1,16 @@
+package Lecture21;
+
+public class PairBalanced {
+
+	int height;
+	boolean isbalanced;
+
+	PairBalanced() {
+
+	}
+
+	PairBalanced(int height, boolean isbalanced) {
+		this.height = height;
+		this.isbalanced = isbalanced;
+	}
+}
