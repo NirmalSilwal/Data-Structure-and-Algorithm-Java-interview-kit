857a58ab04e9e2de42627a40f3290471d5d0a5d7
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2020-09-28 08:21:06 +0530

    added more operations of hashmap

--- CodingBlocks Training/Day23/HashMapDemo.java
+++ CodingBlocks Training/Day23/HashMapDemo.java
@@ -1,11 +1,14 @@
 package Lecture23;
 
+import java.util.Collection;
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class HashMapDemo {
 
 	public static void main(String[] args) {
-		
+
 		HashMap<String, Integer> map = new HashMap<String, Integer>();
 
 		map.put("Nepal", 100);
@@ -24,16 +27,39 @@ public class HashMapDemo {
 		System.out.println(map.containsKey("USA"));
 
 		System.out.println(map.remove("China")); // null
+
+		map.put("Japan", 300);
+		System.out.println(map);
+		map.put("Japan", 500); // overwrites previous value
+		System.out.println(map);
+
+		System.out.println("\nall keys in hashmap:\n");
+
+		Set<String> allkeys = map.keySet();
+
+		for (String mykey : allkeys) {
+			System.out.println(mykey);
+		}
+
+		System.out.println("\nall values in hashmap:\n");
+
+		Collection<Integer> allvalues = map.values();
+		for (Integer myvalue : allvalues) {
+			System.out.println(myvalue);
+		}
+
+		System.out.println("\nall key and values in hashmap:\n");
+
+		Set<Map.Entry<String, Integer>> allentries = map.entrySet();
+
+		for (Map.Entry<String, Integer> myentry : allentries) {
+			System.out.println(myentry.getKey() + " => " + myentry.getValue());
+		}
 	}
 
 }
 
-/* output:
-{USA=200, Denmark=300, Nepal=100, India=250}
-100
-250
-{USA=200, Denmark=300, Nepal=100}
-false
-true
-null
-*/
+/*
+ * output: {USA=200, Denmark=300, Nepal=100, India=250} 100 250 {USA=200,
+ * Denmark=300, Nepal=100} false true null
+ */
