c360aac70c2989a53eee3867b26c28c83a7737c8
Suraj Kumar pattanaik <44063901+surajpattanaik@users.noreply.github.com>
2020-10-03 22:09:10 +0530

    Update Day4_PowerOfFour.java

--- Leetcode/Leetcode_August_Challenge/Day4_PowerOfFour.java
+++ Leetcode/Leetcode_August_Challenge/Day4_PowerOfFour.java
@@ -42,34 +42,25 @@ public class Day4_PowerOfFour {
 		return Math.floor(logn) == Math.ceil(logn);
 	}
 	
-	//approach 3
-	static boolean isPowerOfFour2(int num) 
-    { 
-        int count = 0; 
-          
-        /*Check if there is  
-        only one bit set in n*/
-        int x = num & (num - 1); 
-          
-        if ( num > 0 && x == 0) 
-        { 
-            /* count 0 bits  
-            before set bit */
-            while(num > 1) 
-            { 
-                num >>= 1; 
-                count += 1; 
-            }  
-          
-            /*If count is even  
-            then return true 
-            else false*/
-            return (count % 2 == 0) ? true : false; 
-        } 
-          
-            /* If there are more than  
-            1 bit set then n is not a 
-            power of 4*/
-        return false; 
-    }  
+	// approach 3
+	static boolean isPowerOfFour2(int num) {
+		int count = 0;
+
+		// Check if there is only one bit set in num
+                int x = num & (num - 1);
+
+		if (num > 0 && x == 0) {
+			// count 0 bits before set bit
+			while (num > 1) {
+				num >>= 1;
+				count += 1;
+			}
+
+			// If count is even then return true else false
+			return (count % 2 == 0) ? true : false;
+		}
+
+		// If there are more than 1 bit set then num is not a power of 4
+		return false;
+	} 
 }
