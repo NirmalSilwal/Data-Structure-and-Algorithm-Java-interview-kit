e25659fe897057ad0f72f0433d21b00525789af7
ifenil <ddobariya001@gmail.com>
2020-10-03 20:08:25 +0530

    Valid_Palindrome

--- Leetcode/Leetcode_August_Challenge/Valid_Palindrome.java
+++ Leetcode/Leetcode_August_Challenge/Valid_Palindrome.java
@@ -3,17 +3,25 @@ package Leetcode_August_Challenge;
 public class Valid_Palindrome {
 
     public boolean isPalindrome(String s) {
-            
+         // check string if it's null no need to go further direct return false
+
             if(s==null){
             return false;
             }
-     
+            
+        // if string contain some char as uppercase or lowercase 
+        // this will convert whole string to lowercase 
         s = s.toLowerCase();
      
+        // pointed i to intially at 0
+        // and j to end of the string (last character) 
         int i=0;
         int j=s.length()-1;
      
+        
         while(i<j){
+            //if i<j and string char contain a-z and 0-9 loop -> true
+
             while(i<j && !((s.charAt(i)>='a' && s.charAt(i)<='z') 
                         || (s.charAt(i)>='0'&&s.charAt(i)<='9'))){
                 i++;
@@ -23,15 +31,16 @@ public class Valid_Palindrome {
                         || (s.charAt(j)>='0'&&s.charAt(j)<='9'))){
                 j--;
             }
-     
+            // if one of string char not match i to j 
+            // it will return false
             if(s.charAt(i) != s.charAt(j)){
                 return false;
             }
-     
+            
             i++;
             j--;
         }
-     
+     // else return true
         return true;
       
         }
