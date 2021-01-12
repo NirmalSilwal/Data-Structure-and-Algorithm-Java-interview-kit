edf9c3cc8a600683faea5d9428fe4aba39a17d7b
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-01-12 15:11:55 +0545

    added another approach, O(n^2) time, O(1) space

--- GeeksForGeeks/Arrays/MaximumSumCircularSubarray.java
+++ GeeksForGeeks/Arrays/MaximumSumCircularSubarray.java
@@ -6,21 +6,31 @@ public class MaximumSumCircularSubarray {
 
 		int[] arr = { 10, 5, -5 };
 		System.out.println(maxSubarraySumCircular(arr)); // 15
+		System.out.println(maxSubarraySumCircular2(arr)); // 15
 
 		int[] arr2 = { 1, -2, 3, -2 };
 		System.out.println(maxSubarraySumCircular(arr2)); // 3
-
-		int[] arr5 = { 5, -3, 5 };
-		System.out.println(maxSubarraySumCircular(arr5)); // 10
+		System.out.println(maxSubarraySumCircular2(arr2)); // 15
 
 		int[] arr3 = { 3, -1, 2, -1 };
 		System.out.println(maxSubarraySumCircular(arr3)); // 4
+		System.out.println(maxSubarraySumCircular2(arr3)); // 15
 
 		int[] arr4 = { 3, -2, 2, -3 };
 		System.out.println(maxSubarraySumCircular(arr4)); // 3
+		System.out.println(maxSubarraySumCircular2(arr4)); // 15
+
+		int[] arr5 = { 5, -3, 5 };
+		System.out.println(maxSubarraySumCircular(arr5)); // 10
+		System.out.println(maxSubarraySumCircular2(arr5)); // 15
 
 		int[] arr6 = { -2, -3, -1 };
 		System.out.println(maxSubarraySumCircular(arr6)); // -1
+		System.out.println(maxSubarraySumCircular2(arr6)); // 15
+
+		int[] arr7 = { 3, -4, 5, 6, -8, 7 };
+		System.out.println(maxSubarraySumCircular(arr7)); // 17
+		System.out.println(maxSubarraySumCircular2(arr7)); // 15
 
 	}
 
@@ -40,4 +50,23 @@ public class MaximumSumCircularSubarray {
 		}
 		return result;
 	}
+
+	// O(n^2) Time | O(1) Space
+	public static int maxSubarraySumCircular2(int[] arr) {
+		int result = arr[0];
+
+		for (int i = 0; i < arr.length; i++) {
+			int currMax = arr[i];
+			int currSum = arr[i];
+
+			for (int j = 1; j < arr.length; j++) {
+				int circularIndex = (i + j) % arr.length;
+				currSum += arr[circularIndex];
+				currMax = Math.max(currSum, currMax);
+			}
+			result = Math.max(result, currMax);
+		}
+		return result;
+	}
+
 }
