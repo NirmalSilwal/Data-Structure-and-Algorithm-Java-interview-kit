9c58a767a0e2529c4be57a75070512534a968a51
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-02-15 03:52:33 +0545

    update: driver code to test Queue implementations using 2 Stacks

--- Interview Prep/section11_Queue/QueueUsingStackClient.java
+++ Interview Prep/section11_Queue/QueueUsingStackClient.java
@@ -4,6 +4,8 @@ public class QueueUsingStackClient {
 
 	public static void main(String[] args) {
 
+		System.out.println("\n----------- enqueue efficient -------\n");
+
 		QueueUsingStackEnqueueEfficient queue = new QueueUsingStackEnqueueEfficient();
 
 		System.out.println("size : " + queue.size());
@@ -14,8 +16,25 @@ public class QueueUsingStackClient {
 
 		queue.enqueue(30);
 		queue.enqueue(1);
+
 		System.out.println("removed: " + queue.dequeue());
 		System.out.println("front: " + queue.front());
+
+		System.out.println("\n----------- dequeue efficient -------\n");
+
+		QueueUsingStackDequeueEfficient queue2 = new QueueUsingStackDequeueEfficient();
+
+		System.out.println("size : " + queue2.size());
+		queue2.enqueue(10);
+		queue2.enqueue(20);
+		System.out.println("front: " + queue2.front());
+		System.out.println("size : " + queue2.size());
+
+		queue2.enqueue(30);
+		queue2.enqueue(1);
+
+		System.out.println("removed: " + queue2.dequeue());
+		System.out.println("front: " + queue2.front());
 
 	}
 
