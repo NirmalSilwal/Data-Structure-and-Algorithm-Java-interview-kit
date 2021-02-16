993419d04746680bf5c392e3468dd12e0da4b3bb
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-02-17 00:16:52 +0545

    driver class to test LinkedList implementation from scratch

---
+++ Interview Prep/section13_LinkedList/LinkedListClient.java
@@ -0,0 +1,70 @@
+package section13_LinkedList;
+
+public class LinkedListClient {
+
+	public static void main(String[] args) throws Exception {
+
+		LinkedList list = new LinkedList();
+
+		list.display();
+		// System.out.println(list.getFirst()); // null pointer exception
+		// System.out.println("\n last node item: " + list.getLast());
+		// System.out.println(list.getAt(2)); // invalid index Exception
+
+		list.addFirst(7);
+		list.display();
+		list.addLast(10);
+		list.addLast(20);
+		list.addLast(30);
+		list.addLast(40);
+
+		list.display();
+
+		list.addFirst(5);
+
+		list.display();
+
+		list.addFirst(4);
+
+		list.display();
+		list.addLast(50);
+		list.display();
+
+		list.addFirst(3);
+		list.display();
+		System.out.println("\nfirst node item: " + list.getFirst());
+		System.out.println("\nlast node item: " + list.getLast());
+
+		System.out.println("\n");
+		list.display();
+
+		System.out.println(list.getAt(2));
+		System.out.println(list.getAt(1));
+		System.out.println(list.getAt(3));
+
+		list.addAt(4, 400);
+		list.display();
+
+		list.addAt(0, 200);
+		list.display();
+
+		System.out.println("after removal");
+
+		System.out.println(list.removeFirst());
+		list.display();
+		System.out.println(list.removeFirst());
+		list.display();
+		System.out.println("removing at last");
+		System.out.println(list.removeLast());
+		list.display();
+		System.out.println(list.removeLast());
+		list.display();
+		System.out.println("removig at index..");
+		System.out.println(list.removeAt(3));
+		list.display();
+		System.out.println(list.removeAt(0));
+		list.display();
+
+	}
+
+}
