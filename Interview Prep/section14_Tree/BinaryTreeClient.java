5b00fdca7a1835648cb195a546be9f612c585970
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-02-24 02:53:21 +0545

    update: added code to test newly added functions

--- Interview Prep/section14_Tree/BinaryTreeClient.java
+++ Interview Prep/section14_Tree/BinaryTreeClient.java
@@ -1,4 +1,14 @@
 package section14_Tree;
+/*
+ * Input sample: binary tree
+ * 
+ * 50 true 25 true 38 false false true 48 true 18 false false false true 45 true 85 false false true 60 false false
+ * 
+ * input sample: binary search tree
+ * 
+ * 50 true 25 true 12 false false true 37 false false true 75 true 62 false false true 87 false false
+ * 
+ */
 
 public class BinaryTreeClient {
 
@@ -7,11 +17,6 @@ public class BinaryTreeClient {
 		BinaryTree btree = new BinaryTree();
 		System.out.println();
 		btree.display();
-
-		/*
-		 * 50 true 25 true 38 false false true 48 true 18 false false false true
-		 * 45 true 85 false false true 60 false false
-		 */
 
 		System.out.println("\nHeight of tree: " + btree.height());
 
@@ -26,6 +31,16 @@ public class BinaryTreeClient {
 
 		System.out.println("\n\nLevel-order traversal");
 		btree.levelOrderTraversal();
+
+		System.out.println("\n\nisBST: " + btree.isBinaryTreeTheBST());
+
+		System.out.println("isBST: " + btree.isBST());
+
+		System.out.println("\nIn-order iterative: ");
+		btree.inOrderIterative();
+
+		System.out.println("\nsum of leafs: " + btree.sumOfLeafs());
+
 	}
 
 }
@@ -33,51 +48,55 @@ public class BinaryTreeClient {
 /* output:
 
 Enter data for root node: 
-50 true 25 true 38 false false true 48 true 18 false false false true 45 true 85 false false true 60 false false
+50 true 25 true 12 false false true 37 false false true 75 true 62 false false true 87 false false
 Is there left child of 50? Press true/false
 Enter data for left node of 50
 Is there left child of 25? Press true/false
 Enter data for left node of 25
-Is there left child of 38? Press true/false
-Is there right child of 38? Press true/false
+Is there left child of 12? Press true/false
+Is there right child of 12? Press true/false
 Is there right child of 25? Press true/false
 Enter data for right node of 25
-Is there left child of 48? Press true/false
-Enter data for left node of 48
-Is there left child of 18? Press true/false
-Is there right child of 18? Press true/false
-Is there right child of 48? Press true/false
+Is there left child of 37? Press true/false
+Is there right child of 37? Press true/false
 Is there right child of 50? Press true/false
 Enter data for right node of 50
-Is there left child of 45? Press true/false
-Enter data for left node of 45
-Is there left child of 85? Press true/false
-Is there right child of 85? Press true/false
-Is there right child of 45? Press true/false
-Enter data for right node of 45
-Is there left child of 60? Press true/false
-Is there right child of 60? Press true/false
+Is there left child of 75? Press true/false
+Enter data for left node of 75
+Is there left child of 62? Press true/false
+Is there right child of 62? Press true/false
+Is there right child of 75? Press true/false
+Enter data for right node of 75
+Is there left child of 87? Press true/false
+Is there right child of 87? Press true/false
 
-25 => 50 <= 45
-38 => 25 <= 48
-NULL => 38 <= NULL
-18 => 48 <= NULL
-NULL => 18 <= NULL
-85 => 45 <= 60
-NULL => 85 <= NULL
-NULL => 60 <= NULL
+25 => 50 <= 75
+12 => 25 <= 37
+NULL => 12 <= NULL
+NULL => 37 <= NULL
+62 => 75 <= 87
+NULL => 62 <= NULL
+NULL => 87 <= NULL
 
-Height of tree: 3
+Height of tree: 2
 
 Pre-order traversal
-50 25 38 48 18 45 85 60 
+50 25 12 37 75 62 87 
 
 In-order traversal
-38 25 18 48 50 85 45 60 
+12 25 37 50 62 75 87 
 
 Post-order traversal
-38 18 48 25 85 60 45 50 
+12 37 25 62 87 75 50 
 
 Level-order traversal
-50 25 45 38 48 85 60 18 
+50 25 75 12 37 62 87 
+
+isBST: true
+isBST: true
+
+In-order iterative: 
+[12, 25, 37, 50, 62, 75, 87]
+
+sum of leafs: 198
 */
