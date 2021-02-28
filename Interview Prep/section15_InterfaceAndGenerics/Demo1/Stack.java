60d27e09a0b647b0de1da062b2ff4030f4b0438d
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-02-28 21:12:21 +0545

    stack class that implements dynamic stack interface

---
+++ Interview Prep/section15_InterfaceAndGenerics/Demo1/Stack.java
@@ -0,0 +1,41 @@
+package section15_InterfaceAndGenerics;
+
+public class Stack implements DynamicStackInterface {
+
+	/* (non-Javadoc)
+	 * @see section15_InterfaceAndGenerics.Stack_Interface#push(int)
+	 */
+	public void push(int item) {
+		
+	}
+
+	/* (non-Javadoc)
+	 * @see section15_InterfaceAndGenerics.Stack_Interface#pop()
+	 */
+	public int pop() {
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see section15_InterfaceAndGenerics.DSAInterface#size()
+	 */
+	public int size() {
+		return 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see section15_InterfaceAndGenerics.DSAInterface#isEmpty()
+	 */
+	public boolean isEmpty() {
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see section15_InterfaceAndGenerics.DynamicStackInterface#display()
+	 */
+	public void display() {
+		
+	}
+
+
+}
