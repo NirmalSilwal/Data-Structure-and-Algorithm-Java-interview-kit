98c42afe92fe049f87f374646ff7bcb3e56a9976
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-03-07 03:18:42 +0545

    update: driver code to test hashtable functions

--- Interview Prep/section16_Hashmap/HashTableClient.java
+++ Interview Prep/section16_Hashmap/HashTableClient.java
@@ -4,17 +4,36 @@ public class HashTableClient {
 
 	public static void main(String[] args) throws Exception {
 
-		HashTable<String, Integer> map = new HashTable<>(5);
+		HashTable<String, Integer> map = new HashTable<>(4);
 		map.put("USA", 1000);
 		map.put("Nepal", 50);
 		map.put("India", 200);
-		map.put("Germany", 500);
-		map.put("Canda", 600);
+		map.put("India", 300);
+		
+//		map.put("Canada", 600);
 		map.display();
 
-		map.put("Nepal", 5000);
 		System.out.println();
+		map.put("Germany", 500);
+		map.put("Nepal", 5000);
+		map.put("Korea", 290);
+//		System.out.println();
 		map.display();
+
+//		System.out.println();
+//		System.out.println(map.get("Nepal"));
+//		System.out.println(map.get("USA"));
+//		System.out.println(map.get("Hongkong"));
+//		map.display();
+//
+//		System.out.println();
+//		System.out.println("removed: " + map.remove("India"));
+//		System.out.println();
+//		map.display();
+//		System.out.println("removed: " + map.remove("Germany"));
+//		System.out.println();
+//		map.display();
+
 	}
 
 }
