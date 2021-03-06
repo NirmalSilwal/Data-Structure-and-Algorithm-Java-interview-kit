c412dd0ecde5d41cdf699c62d46504b0b2e2a38c
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-03-07 03:18:05 +0545

    update: added more functions and optimized the code to constant time

--- Interview Prep/section16_Hashmap/HashTable.java
+++ Interview Prep/section16_Hashmap/HashTable.java
@@ -47,13 +47,80 @@ public class HashTable<K, V> {
 			this.bucketArray[baseIndex] = bucket;
 			this.size++;
 		} else {
-			int findAt = bucket.find(pairToBeAdded);
-			if (findAt == -1) {
+			int foundIndex = bucket.find(pairToBeAdded);
+			if (foundIndex == -1) {
 				bucket.addLast(pairToBeAdded);
 				this.size++;
 			} else {
-				HTPair pair = bucket.getAt(findAt);
+				HTPair pair = bucket.getAt(foundIndex);
 				pair.value = value;
+			}
+		}
+
+		// to make it constant time operation
+
+		double lambda = (this.size * 1.0) / this.bucketArray.length;
+		if (lambda > 0.75) {
+			this.rehash();
+		}
+	}
+
+	private void rehash() throws Exception {
+		GenericLinkedList<HTPair>[] oldBucketArray = this.bucketArray;
+		this.bucketArray = (GenericLinkedList<HTPair>[]) new GenericLinkedList[this.bucketArray.length * 2];
+		this.size = 0;
+
+		for (GenericLinkedList<HTPair> pair : oldBucketArray) {
+			while (pair != null && !pair.isEmpty()) {
+				HTPair currentPair = pair.removeFirst();
+				this.put(currentPair.key, currentPair.value);
+			}
+		}
+
+	}
+
+	// O(1) Time
+	private int hashFunction(K key) {
+		int hashCode = key.hashCode();
+		hashCode = Math.abs(hashCode);
+		int hashIndex = hashCode % this.bucketArray.length;
+		return hashIndex;
+	}
+
+	public V get(K key) throws Exception {
+		int hashIndex = hashFunction(key);
+		GenericLinkedList<HTPair> bucket = this.bucketArray[hashIndex];
+		HTPair pairToFind = new HTPair(key, null);
+
+		if (bucket == null) {
+			return null;
+		} else {
+			int foundIndex = bucket.find(pairToFind);
+			if (foundIndex == -1) {
+				return null;
+			} else {
+				HTPair pair = bucket.getAt(foundIndex);
+				return pair.value;
+			}
+		}
+	}
+
+	public V remove(K key) throws Exception {
+		int hashIndex = hashFunction(key);
+		GenericLinkedList<HTPair> bucket = this.bucketArray[hashIndex];
+		HTPair pairToRemove = new HTPair(key, null);
+
+		if (bucket == null) {
+			return null;
+		} else {
+			int foundIndex = bucket.find(pairToRemove);
+			if (foundIndex == -1) {
+				return null;
+			} else {
+				HTPair pair = bucket.getAt(foundIndex);
+				bucket.removeAt(foundIndex);
+				this.size--;
+				return pair.value;
 			}
 		}
 	}
@@ -64,16 +131,9 @@ public class HashTable<K, V> {
 				bucket.display();
 			} else {
 				System.out.println("NULL");
-				// System.out.println("***********************************");
 			}
 		}
 		System.out.println("----------------------------------------------");
 	}
 
-	private int hashFunction(K key) {
-		int hashCode = key.hashCode();
-		hashCode = Math.abs(hashCode);
-		int hashIndex = hashCode % this.bucketArray.length;
-		return hashIndex;
-	}
 }
