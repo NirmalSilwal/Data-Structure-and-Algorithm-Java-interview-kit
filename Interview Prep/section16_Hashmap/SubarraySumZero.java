55818a2b65b9ae51ba570c76d7052a0444853174
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-03-07 23:52:23 +0545

    finds if subarray sums equal to zero in given array in O(N) time, using prefix sum technique, hashmap

---
+++ Interview Prep/section16_Hashmap/SubarraySumZero.java
@@ -0,0 +1,37 @@
+package section16_Hashmap;
+
+import java.util.HashMap;
+
+public class SubarraySumZero {
+
+	public static void main(String[] args) {
+
+		int[] arr = { 3, -5, -8, 9, -1 }; // true
+
+		System.out.println(subArrayZero(arr));
+	}
+
+	// O(N) Time
+	static boolean subArrayZero(int[] arr) {
+		HashMap<Integer, Boolean> map = new HashMap<>();
+		map.put(arr[0], true);
+
+		int prefixSum = 0;
+		for (int index = 1; index < arr.length; index++) {
+			prefixSum += arr[index];
+			if (map.containsKey(prefixSum))
+				return true;
+			else
+				map.put(prefixSum, true);
+		}
+
+		return false;
+	}
+}
+
+/*
+ * note: prefix sum, P[j] - P[i] = sum[i+1, j] if sum = 0, P[j] == P[i] brute
+ * force approach will be O(N^2)Time, where we find all subarrays and check
+ * their sum equals zero or not. prefix sum till ith index is sum of array from
+ * 0 index to ith index
+ */
