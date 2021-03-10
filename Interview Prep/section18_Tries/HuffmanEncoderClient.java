c0570e1d0858956d54d4fd2e6cf630af28cd664c
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-03-10 22:50:16 +0545

    added code to compare size of String before & after Encoding

--- Interview Prep/section18_Tries/HuffmanEncoderClient.java
+++ Interview Prep/section18_Tries/HuffmanEncoderClient.java
@@ -1,11 +1,15 @@
 package section18_Tries;
 
+import java.util.BitSet;
+
 public class HuffmanEncoderClient {
 
 	public static void main(String[] args) throws Exception {
 
 		String str = "abbcdda";
 		System.out.println("original string: " + str);
+		System.out.println("size of original string is: " + str.length() * 2 + " bytes");
+
 		HuffmanEncoder huffman = new HuffmanEncoder(str);
 
 		String encoded = huffman.encode(str);
@@ -15,15 +19,26 @@ public class HuffmanEncoderClient {
 		System.out.println("decoded string: " + decoded);
 
 		System.out.println(str.equals(decoded));
+
+		BitSet bitset = new BitSet(encoded.length());
+		int bitcounter = 0;
+
+		for (Character ch : encoded.toCharArray()) {
+			if (ch == '1') {
+				bitset.set(bitcounter);
+			}
+			bitcounter++;
+		}
+
+		byte[] arr = bitset.toByteArray();
+		System.out.println("size of encoded string is: " + arr.length + " bytes");
 	}
 
 }
 
-
-/* output:
-
-original string: abbcdda
-encoded string: 00001011001001000
-decoded string: abbcdda
-true
-*/
+/*
+ * output:
+ * 
+ * original string: abbcdda encoded string: 00001011001001000 decoded string:
+ * abbcdda true
+ */
