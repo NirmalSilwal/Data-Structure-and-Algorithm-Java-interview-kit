c8367c21826f89d6e99dd932d21951c9365b1b43
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-03-11 21:50:41 +0545

    update: added iterative DP approach, complexity analysis

--- Interview Prep/section19_DynamicProgramming/Fibonacci.java
+++ Interview Prep/section19_DynamicProgramming/Fibonacci.java
@@ -1,7 +1,5 @@
 package section19_DynamicProgramming;
 
-import java.util.Arrays;
-
 public class Fibonacci {
 
 	public static void main(String[] args) {
@@ -9,9 +7,13 @@ public class Fibonacci {
 		int nth = 7;
 		int[] storage = new int[nth + 1];
 		System.out.println(fibonacci(nth, storage)); // 13
+		System.out.println(fibonacciIterativeDP(nth)); // 13
+		System.out.println(fibonacciNormal(nth)); // 13
+
 	}
 
-	// using memoization
+	// using memoization - recursive approach
+	// O(N) Time | O(N) Space
 	public static int fibonacci(int n, int[] storage) {
 
 		if (n == 0 || n == 1)
@@ -25,7 +27,36 @@ public class Fibonacci {
 		int fibb = fibnm1 + fibnm2;
 
 		storage[n] = fibb;
-		System.out.println(Arrays.toString(storage));
+		// System.out.println(Arrays.toString(storage));
+		return fibb;
+	}
+
+	// DP - iterative storage approach - DP
+	// O(N) Time | O(N) Space
+	public static int fibonacciIterativeDP(int n) {
+		int[] storage = new int[n + 1];
+
+		// seed values
+		storage[0] = 0;
+		storage[1] = 1;
+
+		for (int nth = 2; nth <= n; nth++) {
+			storage[nth] = storage[nth - 1] + storage[nth - 2];
+		}
+
+		return storage[n];
+	}
+
+	// without DP
+	// O(2^N) Time | O(1) Space
+	public static int fibonacciNormal(int n) {
+		if (n == 0 || n == 1)
+			return n;
+
+		int fibnm1 = fibonacciNormal(n - 1);
+		int fibnm2 = fibonacciNormal(n - 2);
+		int fibb = fibnm1 + fibnm2;
+
 		return fibb;
 	}
 }
