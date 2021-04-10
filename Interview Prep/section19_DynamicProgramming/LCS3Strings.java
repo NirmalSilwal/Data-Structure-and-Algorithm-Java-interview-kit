82748d300a634448ed1653c15df27a7b7b98a4f2
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-04-11 05:34:35 +0545

    update: added optimal soln, bottom up DP to find LCS of 3 strings

--- Interview Prep/section19_DynamicProgramming/LCS3Strings.java
+++ Interview Prep/section19_DynamicProgramming/LCS3Strings.java
@@ -5,9 +5,11 @@ public class LCS3Strings {
 	public static void main(String[] args) {
 
 		// String s1 = "abcgd", s2 = "bcgad", s3 = "ad"; // 2
-		String s1 = "geeks", s2 = "geeksfor", s3 = "geeksforgeeks"; // 5
-		// String s1 = "abcausdhbvaauhdpqweasdgd", s2 = "qwaeohfdasasdfqpaew",
-		// s3 = "bnprsdhfasoihqd"; // 2
+		String s1 = "abcd", s2 = "agcfd", s3 = "ad"; // 2
+		// String s1 = "geeks", s2 = "geeksfor", s3 = "geeksforgeeks"; // 5
+		// String s1 = "abc", s2 = "agcfd", s3 = "aeg"; // 1
+		// String s1 = "abcausdhbvaauhdpqweasdgd", s2 = "qwaeohfdasasdfqpaew", s3 = "bnprsdhfasoihqd"; // 2
+		// String s1 = "abeti", s2 = "btje", s3 = "tjjabt";
 
 		System.out.println(lcs3StrRecursion(s1, s2, s3, 0, 0, 0));
 
@@ -23,6 +25,8 @@ public class LCS3Strings {
 		}
 
 		System.out.println(lcs3StrTopDownDP(s1, s2, s3, 0, 0, 0, storage));
+		// bottom up
+		System.out.println(lcs3StrBottomUpDP(s1, s2, s3));
 	}
 
 	public static int lcs3StrRecursion(String s1, String s2, String s3, int vidx1, int vidx2, int vidx3) {
@@ -76,5 +80,38 @@ public class LCS3Strings {
 		storage[vidx1][vidx2][vidx3] = lcsLen;
 
 		return lcsLen;
+	}
+
+	public static int lcs3StrBottomUpDP(String s1, String s2, String s3) {
+		int one = s1.length(), two = s2.length(), three = s3.length();
+		int[][][] storage = new int[one + 1][two + 1][three + 1];
+
+		for (int block = one - 1; block >= 0; block--) {
+
+			for (int row = two - 1; row >= 0; row--) {
+
+				for (int col = three - 1; col >= 0; col--) {
+
+					// logic from top down
+					char ch1 = s1.charAt(block);
+					char ch2 = s2.charAt(row);
+					char ch3 = s3.charAt(col);
+
+					int lcsLen = 0;
+					if (ch1 == ch2 && ch2 == ch3) {
+						lcsLen = 1 + storage[block + 1][row + 1][col + 1];
+					} else {
+						int option1 = storage[block + 1][row][col];
+						int option2 = storage[block][row + 1][col];
+						int option3 = storage[block][row][col + 1];
+
+						lcsLen = Math.max(option1, Math.max(option2, option3));
+					}
+
+					storage[block][row][col] = lcsLen;
+				}
+			}
+		}
+		return storage[0][0][0];
 	}
 }
