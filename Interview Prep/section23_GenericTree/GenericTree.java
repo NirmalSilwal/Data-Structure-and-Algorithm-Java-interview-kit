b615bfaf2c0f96a6b6256d1e450cae0377647293
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-04-27 12:01:54 +0545

    update: find height, display, max item, search item in Tree

--- Interview Prep/section23_GenericTree/GenericTree.java
+++ Interview Prep/section23_GenericTree/GenericTree.java
@@ -21,15 +21,15 @@ public class GenericTree {
 	}
 
 	private Node construct(Node parent, int ith) {
-		
+
 		// root node
 		if (parent == null) {
 			System.out.println("Enter the data for root node?");
 		} else {
 			// prompt
-			System.out.println("Enter the data for " + ith + " child of " + parent.data + "?");
+			System.out.println("Enter the data for " + ith + "th child of " + parent.data + "?");
 		}
-		
+
 		// take node data
 		int item = sc.nextInt();
 
@@ -49,5 +49,126 @@ public class GenericTree {
 		}
 
 		return newNode;
+	}
+
+	public void display() {
+		System.out.println("------------------------------------------\n");
+		this.display(this.root);
+		System.out.println("------------------------------------------\n");
+	}
+
+	private void display(Node node) {
+
+		// self-work
+		String ans = node.data + " => ";
+		// loop childrens of current node
+		for (Node child : node.children) {
+			ans += child.data + " ";
+		}
+
+		ans += " . ";
+		System.out.println(ans);
+
+		// do the work for smaller problems
+		for (Node child : node.children) {
+			display(child);
+		}
+	}
+
+	public int size() {
+		return size(root);
+	}
+
+	private int size(Node node) {
+		int totalSize = 0;
+
+		// count total children of current node
+		for (Node child : node.children) {
+			int childSize = size(child);
+			totalSize += childSize;
+		}
+
+		return totalSize + 1; // +1 for parent node
+	}
+
+	public int max() {
+		return max(root);
+	}
+
+	/*
+	 * private int max(Node node) {
+	 * 
+	 * int currentMax = Integer.MIN_VALUE; int maxChild = Integer.MIN_VALUE;
+	 * 
+	 * for (Node child : node.children) { maxChild = max(child); }
+	 * 
+	 * if (maxChild > currentMax) { currentMax = maxChild; }
+	 * 
+	 * return Math.max(currentMax, node.data); }
+	 */
+
+	private int max(Node node) {
+
+		int totalMax = node.data;
+
+		for (Node child : node.children) {
+
+			int childMax = max(child);
+
+			if (childMax > totalMax) {
+				totalMax = childMax;
+			}
+		}
+
+		return totalMax;
+	}
+
+	public boolean find(int item) {
+		return find(root, item);
+	}
+
+	private boolean find(Node node, int item) {
+
+		if (node.data == item) {
+			return true;
+		}
+
+		for (Node child : node.children) {
+
+			boolean childResult = find(child, item);
+
+			if (childResult) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public int height() {
+		return height(root);
+	}
+
+	private int height(Node node) {
+
+		// leaf node
+		if (node.children.size() == 0) {
+			return 0;
+		}
+		
+		// can remove base case by initializing maxHeight by -1
+		int maxHeight = 0;
+
+		for (Node child : node.children) {
+
+			int currChildHt = height(child);
+
+			if (currChildHt > maxHeight) {
+				maxHeight = currChildHt;
+			}
+		}
+
+		// +1 for height from root to max height child, level 0 to level 1
+		return maxHeight + 1;
 	}
 }
