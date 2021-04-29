dad79802363900d58bf209d91a6e3671a837f362
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-04-29 12:19:19 +0545

    update: driver code to test new functions in Binary Tree

--- Interview Prep/section24_BinaryTree/BinaryTreeClient.java
+++ Interview Prep/section24_BinaryTree/BinaryTreeClient.java
@@ -24,45 +24,14 @@ public class BinaryTreeClient {
 
 		/* test input: 10 Y 20 Y 40 N N Y 50 N N Y 30 N Y 60 N Y 111 N N */
 		System.out.println("height: " + btree.height());
+
+		/* test input: 10 Y 20 Y 40 N N Y 50 N Y 70 N N Y 30 N Y 60 N N */
+		/* test input: 10 Y 20 Y 40 Y 60 Y 80 N N N N Y 50 N Y 70 N Y 90 N N Y 30 N N */
+
+		System.out.println("diameter: " + btree.diameter());
+		System.out.println("diameter: " + btree.diameter2());
+		System.out.println("diameter: " + btree.diameter3());
+		
+		
 	}
 }
-
-/* output:
-Enter the data for root node: 
-10 Y 20 Y 40 N N Y 580 N N Y 30 N Y 60 N N
-Do you have left child of 10 ?, press Y/N
-Enter the data for left child of 10
-Do you have left child of 20 ?, press Y/N
-Enter the data for left child of 20
-Do you have left child of 40 ?, press Y/N
-Do you have right child of 40 ?, press Y/N
-Do you have right child of 20 ?, press Y/N
-Enter the data for right child of 20
-Do you have left child of 580 ?, press Y/N
-Do you have right child of 580 ?, press Y/N
-Do you have right child of 10 ?, press Y/N
-Enter the data for right child of 10
-Do you have left child of 30 ?, press Y/N
-Do you have right child of 30 ?, press Y/N
-Enter the data for right child of 30
-Do you have left child of 60 ?, press Y/N
-Do you have right child of 60 ?, press Y/N
-
---------------------
-20 => 10 <= 30
-40 => 20 <= 580
-.  => 40 <= .
-.  => 580 <= .
-.  => 30 <= 60
-.  => 60 <= .
---------------------
-size: 6
-max: 580
-found 10: true
-found 50: false
-found 60: true
-found 40: true
-found 500: false
-found -1: false
-height: 2
-*/
