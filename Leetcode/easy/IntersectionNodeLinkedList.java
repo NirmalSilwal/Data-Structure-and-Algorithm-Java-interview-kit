f8e142c9401acf2f8ea37ed9cc53ece0aee8658c
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-05-05 22:21:49 +0545

    LC#160 finds intersection of two linkedlist

---
+++ Leetcode/easy/IntersectionNodeLinkedList.java
@@ -0,0 +1,47 @@
+package easy;
+
+// LC#160 Intersection of Two Linked Lists
+
+public class IntersectionNodeLinkedList {
+
+	private class ListNode {
+		int val;
+		ListNode next;
+
+		public ListNode(int x) {
+			val = x;
+			next = null;
+		}
+
+	}
+
+	public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+		int lenA = getSize(headA), lenB = getSize(headB);
+		// adjust correct start position of pointer
+		while (lenA > lenB) {
+			headA = headA.next;
+			lenA--;
+		}
+
+		while (lenB > lenA) {
+			headB = headB.next;
+			lenB--;
+		}
+
+		while (headA != headB) {
+			headA = headA.next;
+			headB = headB.next;
+		}
+
+		return headA;
+	}
+
+	private static int getSize(ListNode list) {
+		int size = 0;
+		while (list != null) {
+			size++;
+			list = list.next;
+		}
+		return size;
+	}
+}
