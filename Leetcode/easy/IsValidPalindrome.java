fc0c15e2e823902664ebd132f610fd1468f7ea91
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-05-05 13:42:05 +0545

    LC#125:optimal- checks given String is valid palindrome or not

--- Leetcode/easy/IsValidPalindrome.java
+++ Leetcode/easy/IsValidPalindrome.java
@@ -4,7 +4,7 @@ package easy;
 
 public class IsValidPalindrome {
 
-	public static boolean isPalindrome(String s) {
+	public static boolean isPalindrome1(String s) {
 
 		String parsed = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
 
@@ -22,11 +22,53 @@ public class IsValidPalindrome {
 		return true;
 	}
 
+	public static boolean isPalindrome2(String s) {
+
+		String parsed = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+
+		StringBuilder builder = new StringBuilder();
+		builder.append(parsed);
+		builder.reverse();
+
+		String reversed = builder.toString();
+
+		return parsed.equals(reversed);
+	}
+
+	public static boolean isPalindrome(String s) {
+
+		int low = 0, high = s.length() - 1;
+
+		while (low < high) {
+
+			if (!Character.isLetterOrDigit(s.charAt(low))) {
+				low++;
+				continue;
+			}
+			if (!Character.isLetterOrDigit(s.charAt(high))) {
+				high--;
+				continue;
+			}
+
+			if (Character.toLowerCase(s.charAt(low)) != Character.toLowerCase(s.charAt(high))) {
+				return false;
+			}
+			low++;
+			high--;
+		}
+
+		return true;
+	}
+
 	public static void main(String[] args) {
 
 		String s = "A man, a plan, a canal: Panama";
 		System.out.println(isPalindrome(s)); // true
 
 		System.out.println(isPalindrome("race a car")); // false
+
+		System.out.println(isPalindrome("oppo ")); // true
+
 	}
+
 }
