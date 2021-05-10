b666d804146bff8905ddf2e35b2ab857e301d94e
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-05-10 23:01:35 +0545

    LC#242 added another approach, checks if two String are anagrams

--- Leetcode/easy/ValidAnagram242.java
+++ Leetcode/easy/ValidAnagram242.java
@@ -26,10 +26,44 @@ public class ValidAnagram242 {
 		return ans;
 	}
 
+	public static boolean anagram(String s, String t) {
+		if (s.length() != t.length())
+			return false;
+
+		int[] characters = new int[26]; // count of characters in given String
+
+		// increase count of chars at that position in above array for
+		// characters in first string
+		for (char ch : s.toCharArray()) {
+			characters[ch - 'a']++; //
+		}
+
+		// decrease count of chars at that position in above array for
+		// characters in second string
+		for (char ch : t.toCharArray()) {
+			characters[ch - 'a']--; //
+		}
+
+		// now for anagram, count of particular character must be zero as from
+		// first string we increase count, and from second string we decrease
+		// their count
+		for (int count : characters) {
+			if (count != 0)
+				return false;
+		}
+
+		return true;
+	}
+
 	public static void main(String[] args) {
 		String s = "anagram", t = "nagaram";
 		System.out.println(isAnagram(s, t)); // true
 		System.out.println(isAnagram("rat", "car")); // false
+
+		System.out.println(anagram(s, t));
+		System.out.println(anagram("helo", "nrlo"));
+		System.out.println(anagram("rat", "car")); // false
+
 	}
 
 }
