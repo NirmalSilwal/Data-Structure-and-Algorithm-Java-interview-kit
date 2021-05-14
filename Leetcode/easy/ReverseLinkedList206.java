053d0603582a3e09e5ecda00dfb6d44d02bf4247
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-05-14 18:38:59 +0545

    LC#206 Reverse LinkedList (pointers) in O(N) time, O(1) Space

---
+++ Leetcode/easy/ReverseLinkedList206.java
@@ -0,0 +1,34 @@
+package easy;
+
+public class ReverseLinkedList206 {
+
+	private class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode() {
+		}
+
+		ListNode(int val) {
+			this.val = val;
+		}
+
+		ListNode(int val, ListNode next) {
+			this.val = val;
+			this.next = next;
+		}
+	}
+
+	public ListNode reverseList(ListNode head) {
+
+		ListNode newHead = null;
+
+		while (head != null) {
+			ListNode next = head.next;
+			head.next = newHead;
+			newHead = head;
+			head = next;
+		}
+		return newHead;
+	}
+}
