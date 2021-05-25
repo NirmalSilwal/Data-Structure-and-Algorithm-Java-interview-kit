06432d3b736433df8799a514feb12e8c519da0b6
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-05-25 16:04:14 +0545

    check if you can split watermelon evenly among two friends

---
+++ Codeforces/problemset/rating800/Watermelon4A.java
@@ -0,0 +1,68 @@
+package problemset.rating800;
+
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class Watermelon4A {
+
+	public static void main(String[] args) {
+
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+
+		int weight = sc.nextInt();
+
+		if (weight % 2 == 0 && weight > 2) {
+			out.println("YES");
+		} else {
+			out.println("NO");
+		}
+
+		out.close();
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
