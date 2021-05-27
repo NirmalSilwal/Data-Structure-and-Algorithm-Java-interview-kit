51b4992c9418137c0589fabcbddc66e0079ff428
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-05-28 05:35:55 +0545

    finds two missing number in array, Sum approach, O(N) time, O(1) Space

--- UncodeYoutube/array/TwoMissingNumberInArray.java
+++ UncodeYoutube/array/TwoMissingNumberInArray.java
@@ -20,7 +20,29 @@ public class TwoMissingNumberInArray {
 		}
 	}
 
+	// O(N) time | O(1) Space
+	public static void findMissingTwo(int[] arr) {
+		int n = arr.length + 2;
+
+		int totalSum = n * (n + 1) / 2;
+		int arraySum = 0;
+
+		for (int num : arr) {
+			arraySum += num;
+		}
+		int missingSum = totalSum - arraySum;
+		int missingAverage = missingSum / 2;
+
+		int missingAvgSum = missingAverage * (missingAverage + 1) / 2;
+		int firstMissing = totalSum - missingAvgSum;
+		int secondMissing = missingSum - firstMissing;
+
+		System.out.println(firstMissing);
+		System.out.println(secondMissing);
+	}
+
 	public static void main(String[] args) {
-		printTwoMissingNumbers(new int[] { 2, 1, 3, 5 });
+		// printTwoMissingNumbers(new int[] { 2, 1, 3, 5 });
+		findMissingTwo(new int[] { 2, 1, 4 });
 	}
 }
