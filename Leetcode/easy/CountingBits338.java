2a0326a62a966a68eaec592751d76ffa3af799fc
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-06-23 16:03:48 +0545

    LC#338 optimal, returns array containting bit counts, O(N) time, O(N) Space

--- Leetcode/easy/CountingBits338.java
+++ Leetcode/easy/CountingBits338.java
@@ -26,15 +26,30 @@ public class CountingBits338 {
 		return count;
 	}
 
+	// optimal code using DP
+	public static int[] countBitsDP(int n) {
+		int[] ans = new int[n + 1];
+		ans[0] = 0;
+
+		for (int index = 1; index <= n; index++) {
+			if (index % 2 == 0) { // even
+				ans[index] = ans[index / 2];
+			} else { // odd
+				ans[index] = 1 + ans[index / 2];
+			}
+		}
+		return ans;
+	}
+
 	public static void main(String[] args) {
 		System.out.println(Arrays.toString(countBits(2)));
 		System.out.println(Arrays.toString(countBits(5)));
 		System.out.println(Arrays.toString(countBits(0)));
+		
+		System.out.println("\noptimal\n");
+
+		System.out.println(Arrays.toString(countBitsDP(2)));
+		System.out.println(Arrays.toString(countBitsDP(5)));
+		System.out.println(Arrays.toString(countBitsDP(0)));
 	}
 }
-
-/* output:
-[0, 1, 1]
-[0, 1, 1, 2, 1, 2]
-[0]
-*/
