af5240270da69b10b60531ff111b03b973b0d114
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-06-25 00:49:37 +0545

    LC#953 verifies alien dictionary - correct code, 1ms solution

--- Leetcode/easy/VerifyingAlienDictionary953.java
+++ Leetcode/easy/VerifyingAlienDictionary953.java
@@ -30,6 +30,7 @@ public class VerifyingAlienDictionary953 {
 	}
 
 	private static boolean checkTwoWords(String current, String next, String order) {
+
 		Map<Character, Integer> map = new HashMap<>();
 		for (int i = 1; i <= 26; i++) {
 			map.put(order.charAt(i - 1), i);
@@ -37,6 +38,9 @@ public class VerifyingAlienDictionary953 {
 
 		for (int idx = 0; idx < current.length(); idx++) {
 			char ch1 = current.charAt(idx);
+			// if not matched yet but next string is traversed already
+			if (idx == next.length())
+				return false;
 			char ch2 = next.charAt(idx);
 
 			int order1 = map.get(ch1);
@@ -54,9 +58,18 @@ public class VerifyingAlienDictionary953 {
 	}
 
 	public static void main(String[] args) {
-		String[] words = { "hello", "leetcode" };
-		String order = "worldabcefghijkmnpqstuvxyz";
 
+		String[] words2 = { "hello", "leetcode" };
+		String order2 = "hlabcdefgijkmnopqrstuvwxyz";
+		System.out.println(isAlienSorted(words2, order2));
+
+		String[] words = { "word", "world", "row" };
+		String order = "worldabcefghijkmnpqstuvxyz";
 		System.out.println(isAlienSorted(words, order));
+
+		String[] words3 = { "apple", "app" };
+		// String[] words3 = { "apap", "app" };
+		String order3 = "abcdefghijklmnopqrstuvwxyz";
+		System.out.println(isAlienSorted(words3, order3));
 	}
 }
