e46e709cfd149d8c093b61718225b2b79beb7bb6
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-06-27 20:45:28 +0545

    codechef starters June challenge 2021, second problem solution

---
+++ Codechef/StartersJune2021/B.java
@@ -0,0 +1,80 @@
+package StartersJune2021;
+
+import java.io.PrintWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class B {
+
+	public static void main(String[] args) {
+
+		FastReader sc = new FastReader();
+		PrintWriter out = new PrintWriter(System.out);
+
+		int t = sc.nextInt();
+		for (int tt = 0; tt < t; tt++) {
+			
+			int n = sc.nextInt();
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			int x = 2 * (180 + n);
+			int y = a + b;
+			
+			out.println(x - y);
+		}
+		out.close();
+	}
+
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+
+		FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+
+		int[] readArray(int n) {
+			int[] temparr = new int[n];
+			for (int i = 0; i < n; i++) {
+				temparr[i] = nextInt();
+			}
+			return temparr;
+		}
+	}
+}
