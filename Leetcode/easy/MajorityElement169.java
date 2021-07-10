516f9c43da1e9be5a2b072eb058116016a2e35d4
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-07-11 01:44:54 +0545

    LC#300, optimized, finds majority element in array, O(N) time, O(1) Space, Moore's voting algo

--- Leetcode/easy/MajorityElement169.java
+++ Leetcode/easy/MajorityElement169.java
@@ -1,11 +1,13 @@
 package easy;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class MajorityElement169 {
 
-	public static int majorityElement(int[] nums) {
+	// O(N) Time | O(N) Space
+	public static int majorityElement1(int[] nums) {
 
 		HashMap<Integer, Integer> map = new HashMap<>();
 
@@ -27,12 +29,54 @@ public class MajorityElement169 {
 		return ans;
 	}
 
+	// O(NlogN) Time | O(1) Space
+	public static int majorityElement2(int[] nums) {
+		Arrays.sort(nums);
+		return nums[nums.length / 2];
+	}
+
+	// optimizing using Boyer-Moore Voting Algorithm
+	// O(N) Time | O(1) Space
+	public static int majorityElement(int[] nums) {
+		
+		int candidate = nums[0];
+		int votes = 1;
+
+		for (int i = 1; i < nums.length; i++) {
+			int curr = nums[i];
+
+			if (curr == candidate) {
+				votes++;
+			} else {
+				votes--;
+			}
+
+			if (votes == 0) {
+				candidate = curr;
+				votes = 1;
+			}
+		}
+
+		// verifying candidate votes count
+		int count = 0;
+		for (int n : nums) {
+			if (n == candidate)
+				count++;
+		}
+
+		if (count > nums.length / 2) {
+			return candidate;
+		} else {
+			return -1;
+		}
+	}
+
 	public static void main(String[] args) {
 		int[] nums = { 2, 2, 1, 1, 1, 2, 2 };
 		System.out.println(majorityElement(nums));
 
 		System.out.println(majorityElement(new int[] { 3, 2, 3 }));
-
+		//
 		System.out.println(majorityElement(new int[] { 2, 2 }));
 
 	}
