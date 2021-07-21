3e4cce3c4930fc1d9d27b88987dd0e1455a5ea73
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-07-22 01:24:32 +0545

    update: added 3rd approach using divide & conquer

--- Basic programs/revision/MaximumSubarrayLC53.java
+++ Basic programs/revision/MaximumSubarrayLC53.java
@@ -21,7 +21,7 @@ public class MaximumSubarrayLC53 {
 	}
 
 	// using DP - O(N) Time | O(N) Space
-	public static int maxSubArray(int[] nums) {
+	public static int maxSubArray2(int[] nums) {
 
 		int[] dp = new int[nums.length];
 		dp[0] = nums[0];
@@ -30,13 +30,53 @@ public class MaximumSubarrayLC53 {
 
 		for (int i = 1; i < nums.length; i++) {
 
-			// dp[i] = (nums[i] > dp[i - 1]) ? nums[i] : nums[i] + dp[i - 1];
 			dp[i] = Math.max(nums[i], nums[i] + dp[i - 1]);
 
 			maxsum = Math.max(maxsum, dp[i]);
 		}
 
 		return maxsum;
+	}
+
+	// using divide and conquer
+	private static int[] numsArr;
+
+	public static int maxSubArray(int[] nums) {
+
+		numsArr = nums;
+
+		return findMaxSubarray(0, numsArr.length - 1);
+	}
+
+	// helper recusive method
+	private static int findMaxSubarray(int left, int right) {
+		if (left > right)
+			return Integer.MIN_VALUE;
+
+		int mid = left + (right - left) / 2;
+		int currSum = 0;
+		int leftMaxSum = 0, rightMaxSum = 0;
+
+		// finding max subarray sum in left half
+		for (int i = mid - 1; i >= left; i--) {
+			currSum += numsArr[i];
+			leftMaxSum = Math.max(leftMaxSum, currSum);
+		}
+
+		// finding in right half
+		currSum = 0;
+		for (int i = mid + 1; i <= right; i++) {
+			currSum += numsArr[i];
+			rightMaxSum = Math.max(rightMaxSum, currSum);
+		}
+		// considering middle element also
+		int combinedSum = numsArr[mid] + leftMaxSum + rightMaxSum;
+
+		int leftHalfSum = findMaxSubarray(left, mid - 1);
+		int rightHalfSum = findMaxSubarray(mid + 1, right);
+
+		int result = Math.max(combinedSum, Math.max(leftHalfSum, rightHalfSum));
+		return result;
 	}
 
 	public static void main(String[] args) {
