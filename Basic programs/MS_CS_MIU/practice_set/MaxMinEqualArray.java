5d8486d0a42baab63e93ef6d90ab518d6ce6729d
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-09-03 22:07:35 +0530

    fix bug: update iteration condition
    loop from 0th index

--- Basic programs/MS_CS_MIU/practice_set/MaxMinEqualArray.java
+++ Basic programs/MS_CS_MIU/practice_set/MaxMinEqualArray.java
@@ -30,7 +30,7 @@ public class MaxMinEqualArray {
 		int maxValue = arr[0];
 		int minValue = arr[1];
 
-		for (int i = 2; i < arr.length; i++) {
+		for (int i = 0; i < arr.length; i++) {
 			if (arr[i] > maxValue) {
 				maxValue = arr[i];
 			}
