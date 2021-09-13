e4429f0a08aa6ea297dcc45b9dea6867d16e0302
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-09-13 12:56:12 +0545

    update: LC#617: merge two binary trees,added iterative approach

--- Leetcode/MustDoEasyList/MergeTwoBinaryTrees617.java
+++ Leetcode/MustDoEasyList/MergeTwoBinaryTrees617.java
@@ -1,5 +1,8 @@
 package MustDoEasyList;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class TreeNode {
 	int val;
 	TreeNode left;
@@ -21,7 +24,9 @@ class TreeNode {
 
 public class MergeTwoBinaryTrees617 {
 
+	// recursive approach
 	public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+
 		if (root1 == null && root2 == null)
 			return null;
 		if (root1 == null && root2 != null)
@@ -38,5 +43,52 @@ public class MergeTwoBinaryTrees617 {
 		merged.right = mergeTrees(root1.right, root2.right);
 
 		return merged;
+	}
+
+	// iterative approach, using BFS
+	public TreeNode mergeTreesItr(TreeNode root1, TreeNode root2) {
+		if (root1 == null && root2 == null)
+			return root1;
+		if (root1 == null && root2 != null)
+			return root2;
+		if (root2 == null && root1 != null)
+			return root1;
+
+		// Queue implementation using LinkedList
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.add(root1);
+		queue.add(root2);
+
+		while (!queue.isEmpty()) {
+			TreeNode a = queue.poll(); // root1 as merged tree
+			TreeNode b = queue.poll();
+
+			a.val = a.val + b.val;
+
+			if (a.left != null && b.left != null) {
+				queue.add(a.left);
+				queue.add(b.left);
+			}
+
+			if (a.right != null && b.right != null) {
+				queue.add(a.right);
+				queue.add(b.right);
+			}
+
+			if (a.left == null && b.left != null) {
+				a.left = b.left;
+				queue.add(b.left);
+				queue.add(new TreeNode(0));
+			}
+
+			if (a.right == null && b.right != null) {
+				a.right = b.right;
+				queue.add(b.right);
+				queue.add(new TreeNode(0));
+			}
+			// not checking with b.left and b.right as our final tree is 'a'
+		}
+
+		return root1;
 	}
 }
