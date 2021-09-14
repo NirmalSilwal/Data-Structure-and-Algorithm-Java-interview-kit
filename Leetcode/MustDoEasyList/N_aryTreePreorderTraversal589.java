ff7733f53d1e3e46b7672b797219f838f412e41c
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-09-14 11:35:49 +0545

    update: LC#589 added iterative approach, pre-order traversal of N-ary Tree

--- Leetcode/MustDoEasyList/N_aryTreePreorderTraversal589.java
+++ Leetcode/MustDoEasyList/N_aryTreePreorderTraversal589.java
@@ -2,6 +2,7 @@ package MustDoEasyList;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 class Node {
 	public int val;
@@ -22,6 +23,7 @@ class Node {
 
 public class N_aryTreePreorderTraversal589 {
 
+	// recursive solution
 	public List<Integer> answer = new ArrayList<Integer>();
 
 	public List<Integer> preorder(Node root) {
@@ -36,5 +38,29 @@ public class N_aryTreePreorderTraversal589 {
 		}
 
 		return answer;
+	}
+
+	public List<Integer> result = new ArrayList<Integer>();
+
+	public List<Integer> preorderIterative(Node root) {
+
+		if (root == null)
+			return result;
+
+		Stack<Node> stack = new Stack<Node>();
+		stack.push(root);
+
+		while (!stack.isEmpty()) {
+
+			Node curr = stack.pop();
+			result.add(curr.val);
+
+			// inserting children of current node to stack in reverse order
+			for (int i = curr.children.size() - 1; i >= 0; i--) {
+				stack.push(curr.children.get(i));
+			}
+		}
+
+		return result;
 	}
 }
