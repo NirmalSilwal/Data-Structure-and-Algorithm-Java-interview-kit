60e994741b0c8c24710f47cec1dc36b1cb5cd570
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-09-21 17:08:34 +0545

    update: LC#852 optimal, finds peak index in O(logN) time, O(1) space

--- Leetcode/MustDoEasyList/PeakIndexInMountainArray852.java
+++ Leetcode/MustDoEasyList/PeakIndexInMountainArray852.java
@@ -2,6 +2,7 @@ package MustDoEasyList;
 
 public class PeakIndexInMountainArray852 {
 
+	// O(N) time, O(1) space
 	public static int peakIndexInMountainArray(int[] arr) {
 
 		for (int i = 1; i < arr.length; i++) {
@@ -13,8 +14,34 @@ public class PeakIndexInMountainArray852 {
 		return arr.length - 1;
 	}
 
+	// 2nd approach, O(LogN) time, O(1) space
+	public static int peakIndexInMountainArray2(int[] arr) {
+
+		int low = 0, high = arr.length - 1;
+		int ans = 0;
+
+		while (low < high) {
+
+			int mid = low + (high - low) / 2;
+
+			if (arr[mid] > arr[mid + 1]) {
+				ans = mid;
+				high = mid;
+			} else {
+				ans = mid + 1;
+				low = mid + 1;
+			}
+		}
+		return ans;
+	}
+
 	public static void main(String[] args) {
 		int[] arr = { 3, 4, 5, 1 };
-		System.out.println(peakIndexInMountainArray(arr));
+		int[] arr2 = { 24, 69, 100, 99, 79, 78, 67, 36, 26, 19 };
+		int[] arr3 = { 40, 48, 61, 75, 100, 99, 98, 39, 30, 10 };
+
+		System.out.println(peakIndexInMountainArray2(arr3));
+		System.out.println(peakIndexInMountainArray2(arr2));
+		System.out.println(peakIndexInMountainArray2(arr));
 	}
 }
