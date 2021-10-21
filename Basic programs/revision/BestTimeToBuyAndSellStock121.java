72c6a9a4c999dac7b9827d686b7315a20ead0217
Nirmal Silwal <nirmaldeepsil.11@gmail.com>
2021-10-21 11:42:27 +0545

    LC#121 added DP approach, best time to buy & sell stock problem

--- Basic programs/revision/BestTimeToBuyAndSellStock121.java
+++ Basic programs/revision/BestTimeToBuyAndSellStock121.java
@@ -12,6 +12,7 @@ public class BestTimeToBuyAndSellStock121 {
 	public static int maxProfit(int[] prices) {
 		if (prices.length < 2)
 			return 0;
+
 		int profit = 0;
 		int currlow = prices[0];
 		int sell = 0;
@@ -27,9 +28,25 @@ public class BestTimeToBuyAndSellStock121 {
 		return profit;
 	}
 
+	public static int maxProfitUsingDP(int[] prices) {
+		int maxProfit = 0;
+		int minPriceSoFar = Integer.MAX_VALUE;
+
+		for (int i = 0; i < prices.length; i++) {
+			minPriceSoFar = Math.min(prices[i], minPriceSoFar);
+			maxProfit = Math.max(maxProfit, prices[i] - minPriceSoFar);
+		}
+
+		return maxProfit;
+	}
+
 	public static void main(String[] args) {
 		int[] prices = { 7, 1, 5, 3, 6, 4 };
 		System.out.println(maxProfit(prices));
 		System.out.println(maxProfit(new int[] { 7, 6, 4, 3, 1 }));
+
+		// test DP approach
+		System.out.println(maxProfitUsingDP(prices));
+		System.out.println(maxProfitUsingDP(new int[] { 7, 6, 4, 3, 1 }));
 	}
 }
