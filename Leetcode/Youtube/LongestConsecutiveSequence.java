f25d5de6c9315756b0c58ceede68ce5fbecaef3c
NirmalSilwal <nirmaldeepsil.11@gmail.com>
2022-02-21 05:21:43 -0600

    O(NlogN) time-finds longest consecutive sequence in given array

--- Leetcode/Youtube/LongestConsecutiveSequence.java
+++ Leetcode/Youtube/LongestConsecutiveSequence.java
@@ -1,5 +1,6 @@
 package Leetcode.Youtube;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -33,13 +34,38 @@ public class LongestConsecutiveSequence {
 		return max;
 	}
 
+	// approach 2 using Sorting
+	// O(NlogN) time, O(N) space
+	public static int lengthOfLongestConsecutiveSequence2(int[] arr, int N) {
+		int max = 1;
+		int[] arrclone = arr.clone(); // making copy of original array
+
+		Arrays.sort(arrclone); // sorting array
+
+		int ans = 1;
+		for (int i = 1; i < arrclone.length; i++) {
+			if (arrclone[i - 1] == arrclone[i] - 1) {
+				ans++;
+				max = Math.max(ans, max);
+			} else {
+				ans = 1;
+			}
+		}
+
+		return max;
+	}
+
 	public static void main(String[] args) {
 
 //		int[] arr = { 100, 4, 200, 1, 3, 2 }; 
-//		int[] arr = { 1, 5, 9, 13, 6, 12, 7, 11, 14, 15}; 
+//		int[] arr = { 1, 5, 9, 13, 6, 12, 7, 11, 14, 15 };
 		int[] arr = { 9, 5, 4, 9, 10, 10, 6 };
 		int[] arr2 = { 0, 3, 7, 2, 5, 8, 4, 6, 0, 1 };
 		System.out.println(lengthOfLongestConsecutiveSequence(arr, 7));
 		System.out.println(lengthOfLongestConsecutiveSequence(arr2, 9));
+
+		System.out.println(lengthOfLongestConsecutiveSequence2(arr, 7));
+		System.out.println(lengthOfLongestConsecutiveSequence2(arr2, 9));
+
 	}
 }
